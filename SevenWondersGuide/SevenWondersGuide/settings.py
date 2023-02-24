b775df403adaf004146aaddb73e68959765095ea
sequencer <sequencer@o2.pl>
2023-02-24 16:48:01 +0100

    Downgrade to py3.10 for pythonanywhere

--- SevenWondersGuide/SevenWondersGuide/settings.py
+++ SevenWondersGuide/SevenWondersGuide/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'not-so-secret'
+SECRET_KEY = 'django-insecure-change-it'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -127,3 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media directories
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
